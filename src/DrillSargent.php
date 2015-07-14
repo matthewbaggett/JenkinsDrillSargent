d532b3ac3238c44f0d726c882d18207691831c19
Matthew Baggett <matthew@baggett.me>
2015-07-14 14:19:23 +0100

    Detect Jenkins instances using UDP.

--- src/DrillSargent.php
+++ src/DrillSargent.php
@@ -13,14 +13,39 @@ class DrillSargent
         define('SCRIPT_ROOT', $_SERVER['PWD']);
     }
     /**
-     * @return \JenkinsKhan\Jenkins
+     * @return \JenkinsKhan\Jenkins[]
      */
     private function detectJenkinsInstalls()
     {
-        // TODO: Use UDP discovery to find the host.
+        $broadcast_string = "irrelevent";
 
-        $jenkinsHost = "http://jenkins.frontline-solutions.co.uk:8080";
-        $jenkins = new \JenkinsKhan\Jenkins($jenkinsHost);
+        $detectTimeout = 2;
+        $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, 1);
+        socket_sendto($sock, $broadcast_string, strlen($broadcast_string), 0, '255.255.255.255', 33848);
+        socket_set_option($sock,SOL_SOCKET,SO_RCVTIMEO,array("sec"=>$detectTimeout,"usec"=>0));
+
+        //Do some communication, this loop can handle multiple clients
+        $timeStart = microtime(true);
+        $jenkinsXmls = [];
+        echo "Waiting for Jenkins to announce themselves ... \n";
+
+        while(microtime(true) <= ($timeStart + $detectTimeout))
+        {
+            //Receive some data
+            $recv = socket_recvfrom($sock, $buf, 512, 0, $remote_ip, $remote_port);;
+            if($recv){
+                $xml = simplexml_load_string($buf);
+                $jenkinsXmls[] = $xml;
+
+                echo " > Found {$xml->url} ... \n";
+            }
+        }
+        socket_close($sock);
+
+        foreach($jenkinsXmls as $jenkinsConfig) {
+            $jenkins[] = new \JenkinsKhan\Jenkins($jenkinsConfig->url);
+        }
 
         return $jenkins;
 
@@ -30,90 +55,92 @@ class DrillSargent
     {
         $maxWaterUnderBridge = 60*15;
 
-        $jenkins = $this->detectJenkinsInstalls();
-        foreach ($jenkins->getAllJobs() as $jobName) {
-            $jobName = $jobName['name'];
-            echo "Getting Job \"{$jobName}\"\n";
-            $job = $jenkins->getJob($jobName);
-            $jobModel = Models\Job::CreateOrFind($jobName);
-            echo " > Colour is: {$job->getColor()}\n";
-            $builds = $job->getBuilds();
-            $builds = array_reverse($builds);
-            echo " > Has " . count($builds) . " builds\n";
-            $gitRepoPath = SCRIPT_ROOT . "/tmp/" . str_replace(" ", "_", $job->getName());
+        $jenkinses = $this->detectJenkinsInstalls();
+        foreach($jenkinses as $jenkins){
+            foreach ($jenkins->getAllJobs() as $jobName) {
+                $jobName = $jobName['name'];
+                echo "Getting Job \"{$jobName}\"\n";
+                $job = $jenkins->getJob($jobName);
+                $jobModel = Models\Job::CreateOrFind($jobName);
+                echo " > Colour is: {$job->getColor()}\n";
+                $builds = $job->getBuilds();
+                $builds = array_reverse($builds);
+                echo " > Has " . count($builds) . " builds\n";
+                $gitRepoPath = SCRIPT_ROOT . "/tmp/" . str_replace(" ", "_", $job->getName());
 
-            if (count($builds) > 0) {
-                foreach ($builds as $build) {
-                    /** @var \JenkinsKhan\Jenkins\Build $build */
-                    $build = $build->getJenkins()->getBuild($job->getNameURLSafe(), $build->getNumber(), null);
+                if (count($builds) > 0) {
+                    foreach ($builds as $build) {
+                        /** @var \JenkinsKhan\Jenkins\Build $build */
+                        $build = $build->getJenkins()->getBuild($job->getNameURLSafe(), $build->getNumber(), null);
 
-                    if(!file_exists($gitRepoPath)) {
-                        echo " > Cloning Repo to $gitRepoPath";
-                        $gitRepository = \Gitonomy\Git\Admin::cloneTo($gitRepoPath, $build->getGitRemoteURL());
-                        echo " ... Done!\n";
-                    }else{
-                        $gitRepository = new Repository($gitRepoPath);
-                    }
-
-                    #\Kint::dump($build);exit;
-                    if ($build->isRunning()) {
-                        echo " > Skipping running build.\n";
-                        continue;
-                    }
-                    $buildModel = Models\Build::search()
-                      ->where('jenkins_build_id', $build->getNumber())
-                      ->where('job_id', $jobModel->job_id)
-                      ->execOne();
-                    if ($buildModel instanceof Models\Build) {
-                    } else {
-                        $buildModel = new Models\Build();
-                        $buildModel->jenkins_build_id = $build->getNumber();
-                        $buildModel->job_id = $jobModel->job_id;
-                        $buildModel->status = $build->getResult();
-                        $buildModel->comment = $build->getDescription() ? $build->getDescription() : "";
-                        $buildModel->git_branch = $build->getGitBranch();
-                        $buildModel->git_revision = $build->getGitRevision();
-
-                        $previousBuildModel = $buildModel->getPrevious();
-
-                        echo " > New Build: {$build->getNumber()} was a {$build->getResult()} and took {$build->getDuration()} seconds\n";
-                        echo " > Status:   {$build->getResult()}\n";
-                        if ($build->getDescription()) {
-                            echo " > Comment:  {$build->getDescription()}\n";
+                        if (!file_exists($gitRepoPath)) {
+                            echo " > Cloning Repo to $gitRepoPath";
+                            $gitRepository = \Gitonomy\Git\Admin::cloneTo($gitRepoPath, $build->getGitRemoteURL());
+                            echo " ... Done!\n";
+                        } else {
+                            $gitRepository = new Repository($gitRepoPath);
                         }
-                        echo " > Branch:   {$build->getGitBranch()}\n";
-                        echo " > Revision: {$build->getGitRevision()}\n";
-                        $gitCommit = $gitRepository->getCommit($build->getGitRevision());
 
-                        if ($previousBuildModel instanceof Models\Build) {
-                            if ($previousBuildModel->status != $buildModel->status) {
-                                if($previousBuildModel->status == "FAILURE" && $buildModel->status == "SUCCESS") {
-                                    $improvedOrWorsen = "Improved";
-                                }elseif($previousBuildModel->status == "SUCCESS" && $buildModel->status == "FAILURE"){
-                                    $improvedOrWorsen = "Worsen";
-                                }else{
-                                    $improvedOrWorsen = "Unknown";
-                                }
+                        #\Kint::dump($build);exit;
+                        if ($build->isRunning()) {
+                            echo " > Skipping running build.\n";
+                            continue;
+                        }
+                        $buildModel = Models\Build::search()
+                          ->where('jenkins_build_id', $build->getNumber())
+                          ->where('job_id', $jobModel->job_id)
+                          ->execOne();
+                        if ($buildModel instanceof Models\Build) {
+                        } else {
+                            $buildModel = new Models\Build();
+                            $buildModel->jenkins_build_id = $build->getNumber();
+                            $buildModel->job_id = $jobModel->job_id;
+                            $buildModel->status = $build->getResult();
+                            $buildModel->comment = $build->getDescription() ? $build->getDescription() : "";
+                            $buildModel->git_branch = $build->getGitBranch();
+                            $buildModel->git_revision = $build->getGitRevision();
 
-                                echo "  > STATUS CHANGED FROM {$previousBuildModel->status} => {$buildModel->status}\n";
-                                echo "  > It was authored by {$gitCommit->getAuthorName()} ({$gitCommit->getAuthorEmail()})\n";
-                                echo "  > at {$gitCommit->getAuthorDate()->format("Y-m-d H:i:s")}\n";
-                                echo "  > It was committed by {$gitCommit->getCommitterName()} ({$gitCommit->getCommitterEmail()})\n";
-                                echo "  > at {$gitCommit->getCommitterDate()->format("Y-m-d H:i:s")}\n";
-                                echo "  > \"" . trim($gitCommit->getMessage()) . "\"\n";
+                            $previousBuildModel = $buildModel->getPrevious();
 
-                                if($gitCommit->getCommitterDate()->getTimestamp() >= time() - $maxWaterUnderBridge){
-                                    echo "  > Send an email!\n";
-                                    $this->stateChanged($jobModel, $buildModel, $gitCommit, $improvedOrWorsen);
-                                }else{
-                                    echo "  > Too long ago to send an email.\n";
-                                }
-                            } else {
-                                echo "  > STATUS NOT CHANGED: {$buildModel->status}\n";
+                            echo " > New Build: {$build->getNumber()} was a {$build->getResult()} and took {$build->getDuration()} seconds\n";
+                            echo " > Status:   {$build->getResult()}\n";
+                            if ($build->getDescription()) {
+                                echo " > Comment:  {$build->getDescription()}\n";
                             }
+                            echo " > Branch:   {$build->getGitBranch()}\n";
+                            echo " > Revision: {$build->getGitRevision()}\n";
+                            $gitCommit = $gitRepository->getCommit($build->getGitRevision());
+
+                            if ($previousBuildModel instanceof Models\Build) {
+                                if ($previousBuildModel->status != $buildModel->status) {
+                                    if ($previousBuildModel->status == "FAILURE" && $buildModel->status == "SUCCESS") {
+                                        $improvedOrWorsen = "Improved";
+                                    } elseif ($previousBuildModel->status == "SUCCESS" && $buildModel->status == "FAILURE") {
+                                        $improvedOrWorsen = "Worsen";
+                                    } else {
+                                        $improvedOrWorsen = "Unknown";
+                                    }
+
+                                    echo "  > STATUS CHANGED FROM {$previousBuildModel->status} => {$buildModel->status}\n";
+                                    echo "  > It was authored by {$gitCommit->getAuthorName()} ({$gitCommit->getAuthorEmail()})\n";
+                                    echo "  > at {$gitCommit->getAuthorDate()->format("Y-m-d H:i:s")}\n";
+                                    echo "  > It was committed by {$gitCommit->getCommitterName()} ({$gitCommit->getCommitterEmail()})\n";
+                                    echo "  > at {$gitCommit->getCommitterDate()->format("Y-m-d H:i:s")}\n";
+                                    echo "  > \"" . trim($gitCommit->getMessage()) . "\"\n";
+
+                                    if ($gitCommit->getCommitterDate()->getTimestamp() >= time() - $maxWaterUnderBridge) {
+                                        echo "  > Send an email!\n";
+                                        $this->stateChanged($jobModel, $buildModel, $gitCommit, $improvedOrWorsen);
+                                    } else {
+                                        echo "  > Too long ago to send an email.\n";
+                                    }
+                                } else {
+                                    echo "  > STATUS NOT CHANGED: {$buildModel->status}\n";
+                                }
+                            }
+                            $buildModel->save();
+                            echo "\n";
                         }
-                        $buildModel->save();
-                        echo "\n";
                     }
                 }
             }
